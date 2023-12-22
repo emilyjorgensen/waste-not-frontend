cb33c68df2c7a3bd0d8074696be6b1f64caf4e24
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-22 14:33:40 -0600

    Ingredients Index with placeholder data

---
+++ src/IngredientsIndex.jsx
@@ -0,0 +1,13 @@
+export function IngredientsIndex(props) {
+  return (
+    <div>
+      <h1>All ingredients</h1>
+      {props.ingredients.map((ingredient) => (
+        <div key={ingredient.id}>
+          <img src={ingredient.image_url} alt="ingredient image" width="300px" />
+          <h3>{ingredient.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
