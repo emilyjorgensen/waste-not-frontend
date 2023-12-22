09fb29219ec7ce16d62ab728ea08cf1c17a1f3fd
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-22 15:33:44 -0600

    Pantry Index connected to backend

--- src/Content.jsx
+++ src/Content.jsx
@@ -17,20 +17,31 @@ export function Content() {
     });
   };
 
-  const pantry_items = [
-    { id: 1, user_id: 1, ingredient_id: 1, amount: "1 dozen", category: "refrigerator", use_by_date: "Jan 14 2024" },
-    { id: 2, user_id: 1, ingredient_id: 3, amount: "1 package", category: "pantry", use_by_date: "Jan 14 2024" },
-    {
-      id: 3,
-      user_id: 1,
-      ingredient_id: 5,
-      amount: "4 8-oz cuts",
-      category: "refrigerator",
-      use_by_date: "Jan 14 2024",
-    },
-  ];
+  // const pantry_items = [
+  //   { id: 1, user_id: 1, ingredient_id: 1, amount: "1 dozen", category: "refrigerator", use_by_date: "Jan 14 2024" },
+  //   { id: 2, user_id: 1, ingredient_id: 3, amount: "1 package", category: "pantry", use_by_date: "Jan 14 2024" },
+  //   {
+  //     id: 3,
+  //     user_id: 1,
+  //     ingredient_id: 5,
+  //     amount: "4 8-oz cuts",
+  //     category: "refrigerator",
+  //     use_by_date: "Jan 14 2024",
+  //   },
+  // ];
+
+  const [pantry_items, setPantryItems] = useState([]);
+
+  const handleIndexPantry = () => {
+    console.log("handleIndexPantry");
+    axios.get("http://localhost:3000/pantry_items.json").then((response) => {
+      console.log(response.data);
+      setPantryItems(response.data);
+    });
+  };
 
   useEffect(handleIndexIngredients, []);
+  useEffect(handleIndexPantry, []);
 
   return (
     <main>
--- src/PantryIndex.jsx
+++ src/PantryIndex.jsx
@@ -1,7 +1,17 @@
-export function PantryIndex() {
+/* eslint-disable react/prop-types */
+export function PantryIndex(props) {
   return (
     <div>
       <h1>My Pantry:</h1>
+      {props.pantry_items.map((pantry_item) => (
+        <div key={pantry_item.id}>
+          <img src={pantry_item.image_url} alt="ingredient image" width="300px" />
+          <p>Ingredient: {pantry_item.name}</p>
+          <p>Amount: {pantry_item.amount}</p>
+          <p>Category: {pantry_item.category}</p>
+          <p>Use By: {pantry_item.use_by_date}</p>
+        </div>
+      ))}
     </div>
   );
 }
