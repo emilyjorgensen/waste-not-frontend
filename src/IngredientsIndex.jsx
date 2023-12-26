5658d8c448e928cbcea0e82c79a90dd1457e7a06
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-26 11:25:29 -0600

    reset modal to test, no console errors

--- src/IngredientsIndex.jsx
+++ src/IngredientsIndex.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 export function IngredientsIndex(props) {
+  console.log("onShowPantry IS A ", typeof props.onshowPantry); //log the type of onShowPantry
+
   return (
     <div>
       <h1>All ingredients</h1>
       {props.ingredients.map((ingredient) => (
         <div key={ingredient.id}>
           <img src={ingredient.image_url} alt="ingredient image" width="300px" />
-          <button onClick={() => props.onShowPantry(pantry_item)}>{ingredient.name}</button>
+          <h3>{ingredient.name}</h3>
+          <button>Add to Pantry!</button>
         </div>
       ))}
     </div>
