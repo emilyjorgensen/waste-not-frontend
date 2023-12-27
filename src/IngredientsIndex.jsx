6ec3b04f11fbbdfe810ded1893b8fb491ab740ba
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-27 12:45:33 -0600

    makes search case insensitive

--- src/IngredientsIndex.jsx
+++ src/IngredientsIndex.jsx
@@ -1,15 +1,36 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export function IngredientsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div>
+      <form action="search">
+        <input
+          type="search"
+          placeholder="Search"
+          value={searchFilter}
+          onChange={(event) => setSearchFilter(event.target.value)}
+          list="names"
+        />
+        <datalist id="names">
+          {props.ingredients.map((ingredient) => (
+            <option key={ingredient.id} value={ingredient.name} />
+          ))}
+        </datalist>
+        <button type="submit">Search</button>
+      </form>
       <h1>All ingredients</h1>
-      {props.ingredients.map((ingredient) => (
-        <div key={ingredient.id}>
-          <img src={ingredient.image_url} alt="ingredient image" width="300px" />
-          <h3>{ingredient.name}</h3>
-          <button onClick={() => props.onShowIngredient(ingredient)}>Add to Pantry!</button>
-        </div>
-      ))}
+      {props.ingredients
+        .filter((ingredient) => ingredient.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((ingredient) => (
+          <div key={ingredient.id}>
+            <img src={ingredient.image_url} alt="ingredient image" width="300px" />
+            <h3>{ingredient.name}</h3>
+            <button onClick={() => props.onShowIngredient(ingredient)}>Add to Pantry!</button>
+          </div>
+        ))}
     </div>
   );
 }
