6a73a06d939ec70c699f377fed93907a462ef667
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-28 15:21:41 -0600

    delete old signup form

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -60,21 +60,6 @@ export function Signup() {
           </button>
         </div>
       </form>
-      {/* <form>
-        <div>
-          Name: <input name="name" type="text" />
-        </div>
-        <div>
-          Email: <input name="email" type="email" />
-        </div>
-        <div>
-          Password: <input name="password" type="password" />
-        </div>
-        <div>
-          Password confirmation: <input name="password_confirmation" type="password" />
-        </div>
-        <button type="submit">Signup</button>
-      </form> */}
     </div>
   );
 }
