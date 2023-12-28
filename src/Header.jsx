a682b0f7a78fd692af0e6c324b302c0c46e4f6fe
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-28 14:41:36 -0600

    ternary syntax in html for header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,18 @@
 import { Link } from "react-router-dom";
 
 export function Header() {
-  let authenticationLinks;
-  if (localStorage.jwt === undefined) {
-    authenticationLinks = (
-      <>
-        <Link to="/">Home</Link> | <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
-      </>
-    );
-  } else {
-    authenticationLinks = (
-      <>
-        <Link to="/">Home</Link> | <Link to="/logout">Logout</Link>
-      </>
-    );
-  }
-
   return (
     <header>
-      <nav>{authenticationLinks}</nav>
+      <nav>
+        <Link to="/">Home</Link> |
+        {localStorage.jwt === undefined ? (
+          <>
+            <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link>
+          </>
+        ) : (
+          <Link to="/logout">Logout</Link>
+        )}
+      </nav>
     </header>
   );
 }
