dbfb4a3ab9302e268f9fee09ac88034192dea8c1
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-28 21:23:23 -0600

    update form styling on pantry show

--- src/PantryShow.jsx
+++ src/PantryShow.jsx
@@ -14,23 +14,27 @@ export function PantryShow(props) {
     <div>
       <h1>Pantry Item Information</h1>
       <img src={props.pantry_item.image_url} alt="pantry item image" width="300px" />
-      <p>Name: {props.pantry_item.name}</p>
-      <p>Category: {props.pantry_item.category}</p>
-      <p>Amount: {props.pantry_item.amount}</p>
-      <p>Use By: {props.pantry_item.use_by_date}</p>
+      <h5>{props.pantry_item.name}</h5>
       <form onSubmit={handleSubmit}>
-        <div>
-          Category: <input defaultValue={props.pantry_item.category} name="category" type="text" />
+        <div className="mb-3">
+          Amount: <input defaultValue={props.pantry_item.amount} name="amount" type="text" className="form-control" />
         </div>
-        <div>
-          Amount: <input defaultValue={props.pantry_item.amount} name="amount" type="text" />
+        <div className="mb-3">
+          Category:{" "}
+          <input defaultValue={props.pantry_item.category} name="category" type="text" className="form-control" />
         </div>
-        <div>
-          Use By: <input defaultValue={props.pantry_item.use_by_date} name="use_by_date" type="text" />
+        <div className="mb-3">
+          Use By:{" "}
+          <input defaultValue={props.pantry_item.use_by_date} name="use_by_date" type="text" className="form-control" />
+          <small>00-00-0000</small>
         </div>
-        <button type="submit">Update pantry item</button>
+        <button type="submit" className="btn btn-secondary">
+          Update pantry item
+        </button>
       </form>
-      <button onClick={handleClick}>Destroy Pantry Item</button>
+      <button className="btn btn-danger" onClick={handleClick}>
+        Destroy Pantry Item
+      </button>
     </div>
   );
 }
