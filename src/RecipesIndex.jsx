55ac903b3d90a0f43fc82820e1519d9c0b63cb33
Emily Jorgensen <emjorg10@gmail.com>
2024-04-09 20:56:46 -0500

    Save popular recipe data to local storage to reduce API fetch calls

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -8,12 +8,19 @@ export function RecipesIndex() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    console.log(data.recipes);
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${import.meta.env.VITE_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      console.log(data.recipes);
+      setPopular(data.recipes);
+    }
   };
 
   return (
