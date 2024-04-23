5cb0f9e77c2d438f8d46bf7d5e20c9b1dc7323e0
Emily Jorgensen <emjorg10@gmail.com>
2024-04-22 21:22:03 -0500

    ingredients rendered using optional chaining to safely access array

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -11,6 +11,7 @@ export function Recipe() {
     );
     const detailData = await data.json();
     setDetails(detailData);
+    console.log(detailData);
   };
 
   useEffect(() => {
@@ -21,13 +22,22 @@ export function Recipe() {
     <div className="container">
       <h1>{details.title}</h1>
       <div>
-        <img src={details.image} alt={details.title} />
+        <img src={details.image} alt={details.title || "recipe image"} />
       </div>
       <div>
         <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
       </div>
       <div>
         <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+      </div>
+      <div>
+        {details.extendedIngredients?.length > 0 && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
