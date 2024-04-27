9ac166ff4f223b33c35b793123385ecd2c561ee1
Emily Jorgensen <emjorg10@gmail.com>
2024-04-27 14:15:14 -0500

    Recipe Page styling

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { uniqBy } from "lodash";
 
 export function Recipe() {
   const [details, setDetails] = useState({});
@@ -20,24 +21,27 @@ export function Recipe() {
 
   return (
     <div className="container">
-      <h1>{details.title}</h1>
+      <h1 className="display-3">{details.title}</h1>
       <div>
-        <img src={details.image} alt={details.title || "recipe image"} />
+        <img src={details.image} alt={details.title || "recipe image"} className="pb-3" />
       </div>
+      <h3>About the Recipe:</h3>
       <div>
-        <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+        <p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
       </div>
-      <div>
-        <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
-      </div>
+      <h3>Ingredients:</h3>
       <div>
         {details.extendedIngredients?.length > 0 && (
           <ul>
-            {details.extendedIngredients.map((ingredient) => (
+            {uniqBy(details.extendedIngredients, "id").map((ingredient) => (
               <li key={ingredient.id}>{ingredient.original}</li>
             ))}
           </ul>
         )}
+      </div>
+      <h3>Instructions:</h3>
+      <div>
+        <p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
       </div>
     </div>
   );
